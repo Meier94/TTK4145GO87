a9902c73f64dd67af142116abe729df3655259b4
Amund Marton <pamarton@stud.ntnu.no>
2018-02-10 18:08:02 +0100

    Endret list til map og gjort ting litt mer oversiktlig. Funker ikke enda tho

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # TTK4145GO87
 Go heisprosjekt
+
+https://blog.golang.org/c-go-cgo
+https://github.com/TTK4145/Project
--- network/tcpmod/tcpmod.go
+++ network/tcpmod/tcpmod.go
@@ -13,6 +13,151 @@ import (
 )
 
 
+type msg_t struct{
+	msgId int
+	length int
+	data []byte
+}
+
+type client struct{
+	var conn net.Conn
+	var recv chan msg_t
+	var send chan msg_t
+	
+}
+
+
+func ClientListen(c client){
+	num_rcvd := 0
+	num_sent := 0
+
+	client_c = make(chan *msg_t)
+	talks_m := make(map[int]chan *msg_t)
+
+	//Tcp receiver, passes incoming messages to client_c
+	go TcpListen(&client, &client_c)
+
+	for {
+		select {
+			//TcpListen has received a message from client
+			case newMsg := <- client_c {
+				//Notify correct protocol
+				if !notifyTalk(talks_m, newMsg){
+					new_c := make(chan *msg_t)
+					go runProtocol(msg, new_c, c)
+					talks_m[msg.pId] c = new_c
+				}
+			}
+			case dc <- listen_c {
+				//Client is non responsive
+				//Distribute his orders
+				//Notify talks_m
+				//Return
+			}
+		}
+	}
+
+}
+
+func notifyTalk(talks_m map[int]int, msg *msg_t){
+	recvChan := talks_m[msg.pId]
+	if recvChan == nil {
+		return 0
+	}
+	recvChan <- newMsg
+	return 1
+}
+
+
+//testfunksjon som ikke er så bra lenger i guess viser though en annen måte å lese på pluss input fra bruker
+//Ikke testet fullstendig. Lagt til timeoutsjekk som er helt ny
+//Må lage message translation funksjon
+func TcpListen(c *client, notify chan<- *msg_t){
+	for {
+		c.conn.SetReadDeadline(time.Now().Add(10 * time.Millisecond))
+		n, err := c.conn.Read([]byte(buf))
+		if err != nil {
+			if err, ok := err.(net.Error); ok && err.Timeout(){
+				continue
+			}
+			fmt.Printf("Read fail: %d,%s, count: %d\n", n, err, count)
+			return
+		}
+
+		//Translate into msg format
+		msg := Translate(buf)
+
+		notify <- msg
+
+	}
+}
+
+
+//Ganske experimental nedenfor
+
+
+func runProtocol(m *msg_t, talk_c <-chan *msg_t, c *client){
+	rcvMsgId := m.msgId
+	sndMsgId := 0
+	var msg msg_t
+	switch m.msgId{
+		case HANDLE_ORDER:{
+			status_c = addOrder(m.data)
+			//compose answer
+			var msg msg_t = {pId = m.pId, msgId = sndMsgId, data = ACK}
+			//send some message (twice for redundancy)
+			client.conn.Write(msg)
+			client.conn.Write(msg)
+			//wait for reply
+			for {
+				select {
+				case status := <- status_c:
+					msg.msgId++
+					msg.data = COMPLETE
+					client.conn.Write(msg)
+					//TODO: wait for ack
+				case msg <- talk_c:
+					if msg.msgId == rcvMsgID{
+						client.conn.Write(msg)
+					}
+					else if msg.data == COMPLETE{
+						sm->removeOrder()
+						return;
+					}
+
+				}
+			}
+
+
+			msg <- talk_c
+
+
+			//interpret
+			dosomething
+
+			//terminate protocol
+			
+
+		}
+		case ALIVE_PING:{
+
+		}
+	}
+}
+
+func trySend(c *client, m *msg_t, talk_c <-chan *msg_t, lastId int){
+	//send some message
+	client.conn.Write(msg)
+	//wait for reply
+	msg <- talk_c
+
+	if msg.msgId == rcvMsgID{
+		//rewrite
+	}
+}
+
+
+//ikke i bruk as of now
 func CheckDisconnect(c net.Conn){
 	one := []byte{}
 	c.SetReadDeadline(time.Now())
@@ -24,6 +169,8 @@ func CheckDisconnect(c net.Conn){
 		c.SetReadDeadline(time.Now().Add(10 * time.Millisecond))
 	}
 }
+
+
 
 func Tcp_client(){
 	fmt.Println("Launching server...")
@@ -59,110 +206,6 @@ func Tcp_client(){
 }
 
 
-type msg_t struct{
-	msgId int
-	length int
-	data []byte
-}
-
-type protocolChannel struct{
-	pId int
-	c chan msg_t
-}
-
-type client struct{
-	var conn net.Conn
-	var recv chan msg_t
-	var send chan msg_t
-	
-}
-
-
-func ClientListen(c client){
-	num_rcvd := 0
-	num_sent := 0
-
-	client.recv = make(chan msg_t)
-	client.send = make(chan msg_t)
-
-	l := list.New()
-
-	//Tcp receiver, passes incoming messages to client.recv
-	TcpListen(&client, &l)
-
-	for {
-		select {
-			case recv <- client.recv {
-				go runProtocol(recv, client.conn)
-			}
-			case send <- client.send {
-				go some_protocol(send, client.conn)
-			}
-		}
-	}
-
-}
-
-func runProtocol(m msg_t, recv chan msg_t, c *client){
-	var msg msg_t
-	switch m.msgId{
-		case NEW_ENTRY:{
-
-			//send some message
-			client.conn.Write(msg)
-			//wait for reply
-			msg <- recv
-
-			//interpret
-			dosomething
-
-			//terminate protocol
-			
-
-		}
-		case OTHER_CASE:{
-
-		}
-	}
-
-}
-//testfunksjon som ikke er så bra lenger i guess viser though en annen måte å lese på pluss input fra bruker
-
-//Ikke testet fullstendig. Lagt til timeoutsjekk som er helt ny
-//Må lage message translation funksjon
-func TcpListen(c *client, l *List){
-	for {
-		c.conn.SetReadDeadline(time.Now().Add(10 * time.Millisecond))
-		n, err := c.conn.Read([]byte(buf))
-		if err != nil {
-			if err, ok := err.(net.Error); ok && err.Timeout(){
-				continue
-			}
-			fmt.Printf("Read fail: %d,%s, count: %d\n", n, err, count)
-			return
-		}
-
-		//Translate into msg format
-
-		//Notify protocol routine
-		recvChan := GetChannelById(l, msg.msgId)
-		if recvChan == nil {
-			newchan := make(chan msg_t)
-			go runProtocol(msg, newchan, c)
-			l.PushBack(protocolChannel{pId = msg.msgId, c = newchan})
-		}
-	}
-}
-
-//Ikke testet
-func GetChannelById(l *List, id int){
-	for e := l.Front(); e != nil; e = e.Next() {
-		if e.Value.pId == id {
-			return e.Value.c
-		}
-	}
-	return nil
-}
 
 //Klarer å sende ~215k meldinger frem og tilbake med annen maskin på 30 sek (139 us pr meldingsutveksling (mld + ack))
 func Tcp_server(){
