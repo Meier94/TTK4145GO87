ba42f1f33434362b423ca1232d9372ce7edb17f6
Meier94 <matsmm94@gmail.com>
2018-02-24 07:09:52 +0100

    timing

--- client/client.go
+++ client/client.go
@@ -171,7 +171,7 @@ func notifyTalk(talks_m map[uint32]chan *Msg_t, msg *Msg_t) bool{
 		//Try to forward for 5 ms.
 		select {
 		case recvChan <- msg:
-		case <- time.After(5 * time.Millisecond):
+		case <- time.After(100 * time.Microsecond):
 			//fmt.Println("Couldn't forward message")
 			//This should only happen if an ack message is assumed received
 			//but two tcp messages got lost, and the third message is actually
