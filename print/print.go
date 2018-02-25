043fea475d78089b33a34acf25cbe05510009acb
Tobias Bergkvist <tobias@bergkv.ist>
2018-02-25 20:35:50 +0100

    Print driver ferdig men ikke integrert

--- print/print.go
+++ print/print.go
@@ -4,93 +4,98 @@ package print
 import (
 	"fmt"
 	"sync"
+	"reflect"
+	"container/list"
 )
 
 //Print buffer
 const numStrings = 80
 
 //Allowed static print functions active at a time
-const numStatic = 5
+const numStatic = 10
 
 
 var mut *sync.Mutex
 
-type staticPrint struct {
-	f func() int
-	linesPrinted int
-}
-
 var strings []string
-var stringCount = 0
+var static *list.List
 
-var static []staticPrint
-var staticCount int = 0
-
-var running = false
 
 func Init(){
-	running = true
 	strings = make([]string, 0, numStrings)
-	static = make([]string, 0, numStatic)
+	static = list.New()
 	mut = &sync.Mutex{}
+	fmt.Printf("\n")
 }
 
+//Printf
 func Format(f string, args ...interface{}){
-	if binit {
-		mut.Lock()
-		if stringCount < numStrings {
-			strings[stringCount] = fmt.Sprintf(f, args)
-			stringCount++
-		}
-		mut.Unlock()
-	}
-}
-
-func Line(args ...interface{}){
-	if binit {
-		mut.Lock()
-		if len(strings) < numStrings {
-			strings.append(fmt.Sprintln(args))
-			stringCount++
-		}
-		mut.Unlock()
-	}
-}
-
-func AddStatic(f func() int) bool{
 	mut.Lock()
 	defer mut.Unlock()
-	if len(static) < numStatic {
-		static.append(staticPrint{f, 0})
-		staticCount++
-		return true
+	if len(strings) < numStrings {
+		strings  = append(strings, fmt.Sprintf(f, args...))
 	}
-	return false
+}
+
+//Println
+func Line(args ...interface{}){
+	mut.Lock()
+	defer mut.Unlock()
+	if len(strings) < numStrings {
+		strings = append(strings, fmt.Sprintln(args...))
+	}
+}
+
+//Only one line allowed
+func StaticVars(args ...interface{}) *list.Element {
+	mut.Lock()
+	f := func() int {
+		for _, v := range args {
+			switch t := v.(type){
+			case string:
+				fmt.Print(t)
+			default:
+				fmt.Printf("%v", reflect.Indirect(reflect.ValueOf(v)))
+			}
+		}
+		fmt.Printf("\n")
+		return 1
+	}
+	mut.Unlock()
+	return AddStatic(f)
+}
+
+//Should preferably have fixed print height
+func AddStatic(f func() int) *list.Element {
+	mut.Lock()
+	defer mut.Unlock()
+	return static.PushFront(f)
+}
+
+func RemoveStatic(entry *list.Element){
+	mut.Lock()
+	defer mut.Unlock()
+	static.Remove(entry)
 }
 
 
-func PrintMap() int{
+var linesPrinted = 0
+//Prints buffered prints and calls static print functions
+func Display(){
 	mut.Lock()
-	for s := range static {
-			fmt.Printf("%c[%dA\r",27, s.lines) 	//up n lines
-	}
-	fmt.Printf("%c[J\r",27)			//Clear untill end of screen
+	defer mut.Unlock()
 
+	fmt.Printf("\x1b[%dA\r",linesPrinted) 	//up n lines
+	fmt.Printf("\x1b[J\r")			//Clear untill end of screen
+	linesPrinted = 0
 
-	for s := range strings {
+	for _,s := range strings {
 		fmt.Printf(s)
 	}
-	numStrings = 0
+	strings = strings[:0]
 
-	for s := range static {
-			s.lines = s.f()
+	for e := static.Front(); e != nil; e = e.Next() {
+		printFunc := e.Value.(func() int)
+		linesPrinted += printFunc()
 	}
-
-
-	mut.Unlock()
-	return numlines
 }
-
-
-
-
